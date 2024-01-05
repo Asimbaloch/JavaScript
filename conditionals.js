b600c77994b5994478c3d8929881008a209249de
Asim Khan <asimseom@gmail.com>
2024-01-05 20:46:35 +0500

    adding conditionals

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+//if-else
+
+var marks=31;
+if (marks>30) {
+    console.log ("You are passed")
+}
+
+else {
+    console.log ("Please try again")
+}
+
+//if-else if
+
+var marks=40;
+
+if (marks>40) {
+    console.log ("You have D grade")
+}
+else if (marks>50) {
+    console.log ("You have C Grade")
+}
+
+else if ("marks<33") {
+    console.log ("You try again")
+}
