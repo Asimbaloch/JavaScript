f8ae3f74029ae6eaf076f94643a2bbcda6b6b7a4
Asim Khan <asimseom@gmail.com>
2024-01-05 15:10:38 +0500

    Added comparison and arithmetic operators

---
+++ operators.js
@@ -0,0 +1,12 @@
+//Arithmetic Operators
+
+console.log(5+6)
+console.log(5-6)
+console.log(5/6)
+console.log(5*6)
+console.log (5%6)
+
+//Comparison Operators
+console.log (5>6)
+console.log (5<6)
+console.log (5==6)
