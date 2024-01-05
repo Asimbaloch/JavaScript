869af9dc60577e982efb2d82c943b8d9e968b17c
Asim Khan <asimseom@gmail.com>
2024-01-05 14:28:47 +0500

    I added variables

---
+++ variables.js
@@ -0,0 +1,28 @@
+// Single line comment
+/*I am creating the 
+
+variable*/
+
+var petdog="Rex";
+var petCat="Pepper";
+
+console.log (petdog)
+console.log (petCat)
+
+console.log ("My pet's dog name is :", petdog)
+console.log ("My pet's cat name is :", petCat)
+//Declare another variable and name it catSound. Assign the string of "purr" to it.
+var catSound="purr"
+//Declare another variable and name it dogSound. Assign the string of "woof" to it.
+var dogSound="woof"
+//Console.log the variable petDog, then the string "says", then the variable dogSound.
+console.log (petdog ,"says" ,dogSound)
+//Console.log the variable petCat, then the string "says", then the variable catSound.
+console.log (petCat ,"says" ,catSound)
+//Reassign the value stored in catSound to the string "meow".
+
+var catSound="meow"
+//Console.log the variable petCat, then the string "now says", then the variable catSound.
+
+console.log(petCat, "now says", catSound)
+
