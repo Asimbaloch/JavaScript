1984839fcf2a0780a6bc835e1d438c02f0019e6b
Asim Khan <asimseom@gmail.com>
2024-01-05 20:48:22 +0500

    Added switch statement

--- conditionals.js
+++ conditionals.js
@@ -23,3 +23,21 @@ else if (marks>50) {
 else if ("marks<33") {
     console.log ("You try again")
 }
+
+//switch statement
+//converting the previous if-else example with switch-case
+switch(light) {
+    case 'green':
+        console.log("Drive");
+        break;
+    case 'orange':
+        console.log("Get ready");
+        break;
+    case 'red':
+        console.log("Don't drive");
+        break;
+    default:
+        //this block will run if no condition matches
+        console.log('The light is not green, orange, or red');
+        break;
+ }
