521f0fddbfe5a63f42282628de0ce114d9eef2eb
Asim Khan <asimseom@gmail.com>
2024-01-05 21:34:16 +0500

    I added switch statements

--- conditionals.js
+++ conditionals.js
@@ -43,3 +43,47 @@ switch(light) {
         console.log('The light is not green, orange, or red');
         break;
  }
+
+
+ //Are You Old Enough? 
+ var age=10;
+ if (age>=65) {
+    console.log("You get your income from pension")
+ }
+
+ else if(age<65 && age>=18) {
+    console.log("you get an allowance")
+ }
+
+ else {
+    console.log ("The value of the age variable is not numerical")
+ }
+
+//Days of the week program
+
+var day="Sunday"
+switch(day) {
+    case "Monday" : console.log("Do something");
+    break;
+
+    case "Tuesday": console.log ("Do something");
+    break;
+
+    case 'Wednesday': console.log ("Do something");
+    break;
+
+    case "Thursday" : console.log ("Do something")
+    break;
+
+    case "Friday" :console.log ("Do something");
+    break;
+
+    case 'Saturday':
+        console.log('Do something');
+        break;
+    case 'Sunday':
+        console.log('Do something');
+        break;
+
+        default: console.log ("There is no such day");
+}
