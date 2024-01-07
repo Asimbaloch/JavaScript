e904b1279420a6159c72361483986247761b2e06
Asim Khan <asimseom@gmail.com>
2024-01-07 21:54:23 +0500

    Added two tasks from META course

--- conditionals.js
+++ conditionals.js
@@ -87,3 +87,42 @@ switch(day) {
 
         default: console.log ("There is no such day");
 }
+
+//for loop and conditional
+
+for (var i=1;i<=10;i++)
+{
+    if (i==1) {
+        console.log("Gold Medal")
+    }
+
+    else if (i==2){
+        console.log ("Silver Medal")
+    }
+    else if (i==3){
+        console.log("Bronze Medal")
+    }
+    else {
+        console.log(i)
+    }
+}
+
+//switch statement
+
+
+
+for (var i=1; i<=10;i++)
+switch (i) {
+    case 1 :console.log ("Gold Medal")
+    break;
+
+    case 2: console.log ("Silver Medal")
+    break;
+
+    case 3:console.log ("Bronze Medal")
+    break;
+
+    default:
+        console.log (i)
+
+}    
