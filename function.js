067cc49efea3633f912162f10e725c1dac3c8377
Asim Khan <asimseom@gmail.com>
2024-01-10 20:30:58 +0500

    I added some function

--- function.js
+++ function.js
@@ -19,3 +19,12 @@ function minus(a,b) {
     console.log(c);
 }
 minus();
+
+//function with loop
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i, arr[i])
+    }
+}
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
