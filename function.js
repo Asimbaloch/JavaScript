4a8708be0613b67b817c1d253addcb2ffea11d42
Asim Khan <asimseom@gmail.com>
2024-01-10 19:13:29 +0500

    I added functions

---
+++ function.js
@@ -0,0 +1,21 @@
+//function without Parametres
+
+function add(){
+    var a=3;
+    var b=6;
+    var c=a +b;
+    console.log(c);
+}
+
+add();
+
+//function with Parameters
+function minus(a,b) {
+    var a=3;
+    var b=5;
+    
+    var c=3+5;
+
+    console.log(c);
+}
+minus();
