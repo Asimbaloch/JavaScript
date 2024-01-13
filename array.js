1f76790a6002ee933d1dc6d6d24092044e4da2ab
Asim Khan <asimseom@gmail.com>
2024-01-13 18:28:20 +0500

    Learning Arrays

---
+++ array.js
@@ -0,0 +1,15 @@
+//Array with numbers
+let marks= [93,93,32,54,12]
+console.log(marks)
+console.log(marks.length) //length is property
+
+marks[3]=44  //Numbers are mutable in the Array
+console.log(marks)
+
+//Array with string
+let PM=["Nawaz Sharif","Imran Khan","Shahbaz Sharif"]
+console.log(PM,"Length of the array is", PM.length)
+
+console.log(PM[2]) //strings are not mutable/ immutable
+
+//
--- function.js
+++ function.js
@@ -28,3 +28,18 @@ function listArrayItems(arr) {
 }
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors);
+
+// letterfinder function
+
+function letterFinder(word, match) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            //check if the current characater, word[i], is equal to the match
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+}
+
+letterFinder("test", "t")
