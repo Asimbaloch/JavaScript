a7444466093f2ce6f7e968e7ddf55f95fb63ee03
Asim Khan <asimseom@gmail.com>
2024-01-18 22:45:28 +0500

    I added Object file

---
+++ objects.js
@@ -0,0 +1,15 @@
+//Object
+
+var frontend ={
+    HTML :80,
+    Css :50,
+    Javascript : 40,
+    React :0,
+    Git: 90,
+};
+
+console.log(frontend)
+console.log(frontend.React)
+frontend.Github=80
+
+console.log(frontend)
