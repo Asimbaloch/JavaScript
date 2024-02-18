52f936ea9881ba3ab63f643692c567db1e2d3dcd
Asim Khan <asimseom@gmail.com>
2024-02-18 08:43:44 +0500

    completed if program

---
+++ 1.JS Practice/if.js
@@ -0,0 +1,47 @@
+let age=12;
+
+if (age>20) {
+    console.log('You can visit this site.')
+}
+
+else {
+    console.log('You must be older than 20.')
+}
+
+let time=4;
+
+if (time<12) {
+    console.log('Good Morning')
+}
+else {
+    console.log('Go to work bro.')
+}
+
+//student
+
+let isStudent =false;
+
+if (isStudent){
+    console.log ('You are the student.')
+}
+
+else {
+    console.log('you are not the student')
+}
+
+
+let ages= 22;
+let uniStudent=true;
+
+if (ages>=18) {
+    console.log('You are young now.');
+
+    if(uniStudent) {
+        console.log('you are doing great in life.')
+    }
+}
+
+
+else {
+    console.log('You are teenager.')
+}
