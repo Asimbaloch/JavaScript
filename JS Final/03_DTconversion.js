03f5d512499d972f26f07c56f434751500ea6b03
Asim Khan <asimseom@gmail.com>
2024-02-21 11:37:49 +0500

    conversion of datatypes

---
+++ JS Final/03_DTconversion.js
@@ -0,0 +1,13 @@
+let userAge=33
+
+let booleanUserage=Boolean(userAge)
+
+console.log(typeof booleanUserage);
+
+let userEmail='asimseom@gmail.com'
+
+let numberUseremail=Number(userEmail)
+
+console.log(typeof numberUseremail);
+
+
