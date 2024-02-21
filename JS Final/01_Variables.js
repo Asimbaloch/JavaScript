a0c5691e024d5bc5e59bdedb5b73dabdb5cf9e03
Asim Khan <asimseom@gmail.com>
2024-02-21 09:41:04 +0500

    Added variables and learned the concept of console.table

---
+++ JS Final/01_Variables.js
@@ -0,0 +1,15 @@
+//Variables are the containers to store the vlaues
+
+const userId=332252
+let userEmail='asimseom@gmail.com'
+var userPassword='check'
+
+console.log(userId);
+
+console.table([userEmail,userId,userPassword])
+console.table([typeof(userEmail), typeof(userId), typeof(userPassword)])
+
+/*
+Never use the var variable as it is depreciated now and not considered a good practice.
+
+*/
