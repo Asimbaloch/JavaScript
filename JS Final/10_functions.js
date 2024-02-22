9a0924c043bb6a7e741530db3967af6f55905798
Asim Khan <asimseom@gmail.com>
2024-02-22 12:08:35 +0500

    functions

---
+++ JS Final/10_functions.js
@@ -0,0 +1,18 @@
+function add(num1,num2) {
+    return (num1 + num2)
+}
+
+let addDisplay= add(3,4)
+console.log(`The result of the addDisplay is ${addDisplay}.`)
+
+function userMessage(userName) {
+   if (!userName) {
+    console.log("Please write the user name.");
+   }
+
+   else {
+    console.log(`How are you ${userName}`);
+   }
+} 
+let userPopup= userMessage()
+console.log(userPopup);
